11ffbec9a1289d02d8e68f6fdf71cb9af06fb93a
Finnik <73883383+golkity@users.noreply.github.com>
2024-12-27 16:46:30 +0300

    Update: msg_start; Add: msg_help

---
+++ commands/msg_help.go
@@ -0,0 +1,8 @@
+package commands
+
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+func HelpMesg(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Help commands")
+	bot.Send(msg)
+}
--- commands/msg_start.go
+++ commands/msg_start.go
@@ -2,7 +2,7 @@ package commands
 
 import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
-func Start_com(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет"+update.Message.From.UserName)
+func StartMsg(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome! Your username is "+update.Message.From.UserName)
 	bot.Send(msg)
 }
