093f14922275114a9b9a405761fb5857f0caddc6
Finnik <73883383+golkity@users.noreply.github.com>
2024-12-28 23:56:23 +0300

    Add: keyboards

---
+++ keyboards/inline.go
@@ -0,0 +1,16 @@
+package keyboards
+
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+func StartInline() tgbotapi.InlineKeyboardMarkup {
+	keyboard := tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Мой портфель", "portfel"),
+			tgbotapi.NewInlineKeyboardButtonData("Удалить акцию", "delet"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Добавить акцию", "add"),
+		),
+	)
+	return keyboard
+}
