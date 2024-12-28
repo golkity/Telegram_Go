be0d8b76296563c8164cba6da0d1ae0d527f86bd
Finnik <73883383+golkity@users.noreply.github.com>
2024-12-28 22:25:24 +0300

    update readme.md

--- commands/msg_start.go
+++ commands/msg_start.go
@@ -1,8 +1,12 @@
 package commands
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"tgbot/internal/keyboards"
+)
 
 func StartMsg(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome! Your username is "+update.Message.From.UserName)
+	msg.ReplyMarkup = keyboards.createKeyboard()
 	bot.Send(msg)
 }
