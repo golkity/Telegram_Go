771e31c08ca89057f3ffcdf4bdcaab6981dc2f1a
Finnik <73883383+golkity@users.noreply.github.com>
2025-01-03 15:30:19 +0300

    Add: func analize portfile

--- handlers/handler_keyboard.go
+++ handlers/handler_keyboard.go
@@ -33,6 +33,8 @@ func StartCallback(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 		database.UserStates[callback.From.ID].State = "await_stock_name"
 		msg := tgbotapi.NewMessage(callback.Message.Chat.ID, "Введите название акций:")
 		bot.Send(msg)
+	case "analiz":
+		AnalyzePortfolio(bot, callback.Message)
 	default:
 		log.Printf("Unknown callback data '%s'", callback.Data)
 	}
@@ -107,5 +109,56 @@ func DisplayPortfolio(bot *tgbotapi.BotAPI, chatID int64, userID int64) {
 
 	msg := tgbotapi.NewMessage(chatID, fmt.Sprintf("Общая сумма портфеля: %.2f\nКоличество акций: %d\n\n%s",
 		total, len(portfolio.Stocks), strings.Join(stockDetails, "\n\n")))
+	bot.Send(msg)
+}
+
+func AnalyzePortfolio(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	userID := message.From.ID
+
+	portfolio, exists := database.UserPortfolios[userID]
+	if !exists || len(portfolio.Stocks) == 0 {
+		msg := tgbotapi.NewMessage(message.Chat.ID, "Ваш портфель пуст.")
+		bot.Send(msg)
+		return
+	}
+
+	totalCurrentValue := 0.0
+	totalForecastValue := 0.0
+	positiveTrend := 0
+	negativeTrend := 0
+	neutralTrend := 0
+	stockDetails := []string{}
+
+	for _, stock := range portfolio.Stocks {
+		totalCurrentValue += stock.Price
+
+		forecastedPrice := stock.Price * (1 + stock.Percent/100)
+		totalForecastValue += forecastedPrice
+
+		if stock.Percent > 0 {
+			positiveTrend++
+		} else if stock.Percent < 0 {
+			negativeTrend++
+		} else {
+			neutralTrend++
+		}
+
+		stockDetails = append(stockDetails,
+			fmt.Sprintf("Акция: %s\nТекущая цена: %.2f\nДоходность: %.2f%%\nПрогнозируемая цена: %.2f",
+				stock.Name, stock.Price, stock.Percent, forecastedPrice))
+	}
+
+	trendMessage := fmt.Sprintf("Анализ вашего портфеля:\n\n"+
+		"Общая текущая стоимость портфеля: %.2f\n"+
+		"Прогнозируемая стоимость портфеля: %.2f\n\n"+
+		"Тенденции:\n"+
+		"- Положительная тенденция (рост): %d акций\n"+
+		"- Отрицательная тенденция (падение): %d акций\n"+
+		"- Без изменений: %d акций\n\n"+
+		"Детали акций:\n%s",
+		totalCurrentValue, totalForecastValue, positiveTrend, negativeTrend, neutralTrend,
+		strings.Join(stockDetails, "\n\n"))
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, trendMessage)
 	bot.Send(msg)
 }
